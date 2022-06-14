bab83f53de407ed9a8717f4956f138dcd772ee86
Angel <angelluis.fernandez.morante.contractor@bbva.com>
2022-06-14 16:35:40 +0200

    Inicio

---
+++ src/test/java/org/afernandez/test/springboot/app/SpringbootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.afernandez.test.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
